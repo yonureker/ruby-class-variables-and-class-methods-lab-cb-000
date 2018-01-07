41c2afb4c8961603586645d896bb3a6c50bd6922
Onur Eker <yonureker@gmail.com>
2018-01-07 22:49:43 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << @artist
+    @@genres << @genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count.merge!(@genre="")
+  end
+
+  def self.artist_count
+
+  end
+end
