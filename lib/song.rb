23f0871a830c77e0ca7f4ea4abffd3985cacb045
Onur Eker <yonureker@gmail.com>
2018-01-07 23:21:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,14 @@ class Song
   end
 
   def self.genre_count
-    @@genre_count.merge!(@genre="")
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    @@genre_count
   end
 
   def self.artist_count
