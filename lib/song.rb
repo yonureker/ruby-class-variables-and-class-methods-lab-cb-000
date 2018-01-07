51258b8e8c7ab90ca6a99da7f3f33d3d35f58d8e
Onur Eker <yonureker@gmail.com>
2018-01-07 23:27:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,14 @@ class Song
   end
 
   def self.artist_count
-
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
